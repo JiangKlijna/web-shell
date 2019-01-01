a76b7a83659a6791df1fe6130811962cd83c94e4
jiangKlijna <leil729564090@outlook.com>
2019-01-01 20:00:16 +0800

    :fire:paras

--- setting.go
+++ setting.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 type Parameter struct {
 	Port     int
 	Username string
@@ -7,5 +13,35 @@ type Parameter struct {
 }
 
 func (paras *Parameter) Init() {
+	var (
+		help, version bool
+	)
+	flag.BoolVar(&help, "h", false, "this help")
+	flag.BoolVar(&version, "v", false, "show version and exit")
+	paras.Port = *flag.Int("P", 2019, "listening port")
+	paras.Username = *flag.String("u", "admin", "username")
+	paras.Password = *flag.String("p", "admin", "password")
+	flag.Parse()
+	flag.Usage = usage
+	if help {
+		printVersion()
+		flag.Usage()
+		flag.PrintDefaults()
+	} else if version {
+		printVersion()
+	}
+}
 
+func usage() {
+	fmt.Fprintf(os.Stderr, `Usage: web-shell [-P port] [-u username] [-p password]
+Example: web-shell -P 2019 -u admin -p admin
+
+Options:
+`)
+}
+
+func printVersion() {
+	os.Stderr.WriteString("web-shell version: ")
+	os.Stderr.WriteString(Version)
+	os.Stderr.WriteString("\n")
 }
