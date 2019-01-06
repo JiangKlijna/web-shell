7a89c6e14bee927d3ed497337e55265e87e60371
jiangKlijna <leil729564090@outlook.com>
2019-01-06 19:33:33 +0800

    :cat:add HtmlHandler

--- handler.go
+++ handler.go
@@ -9,6 +9,10 @@ import (
 	"time"
 )
 
+func HtmlHandler() http.Handler {
+	return http.FileServer(http.Dir("html"))
+}
+
 func GetMethodHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
