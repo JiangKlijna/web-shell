322045826c0aff67bae4a5fac5e59b53b309e3ab
jiangKlijna <leil729564090@outlook.com>
2019-01-10 11:13:22 +0800

    :cat:add upgrade func

--- server.go
+++ server.go
@@ -22,9 +22,15 @@ func (s *ShellServer) Init(paras *Parameter) {
 	} else {
 		StaticHandler = MimeHandler(StaticHandler)
 	}
-	s.Handle("/", LoggingHandler(GetMethodHandler(AuthHandler(paras.Username, paras.Password, StaticHandler))))
+	s.Handle("/", s.upgrade(paras, StaticHandler))
 }
 
+// packaging and upgrading http.Handler
+func (s *ShellServer) upgrade(paras *Parameter, h http.Handler) http.Handler {
+	return LoggingHandler(GetMethodHandler(AuthHandler(paras.Username, paras.Password, StaticHandler)))
+}
+
+// run web-shell server
 func (s *ShellServer) Run(paras *Parameter) {
 	err := http.ListenAndServe(":"+strconv.Itoa(paras.Port), s)
 	if err != nil {
