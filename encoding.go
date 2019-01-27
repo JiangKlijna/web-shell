1e4163741a1150d438f7491bec15ab7aad08d8ce
jiangKlijna <leil729564090@outlook.com>
2019-01-27 18:15:18 +0800

    :cat:add InitEncodingIO

--- encoding.go
+++ encoding.go
@@ -7,8 +7,20 @@ import (
 
 var charset *mahonia.Charset
 
+func InitEncodingIO(encoding string) *mahonia.Charset {
+	charset = mahonia.GetCharset(encoding)
+	return charset
+}
+
 type EncodingIO struct {
 	rw io.ReadWriter
+}
+
+func NewEncodingIO(rw io.ReadWriter) io.ReadWriter {
+	if charset.Name == "UTF-8" {
+		return rw
+	}
+	return &EncodingIO{rw}
 }
 
 func (eio *EncodingIO) Write(p []byte) (int, error) {
@@ -19,9 +31,3 @@ func (eio *EncodingIO) Read(p []byte) (int, error) {
 	return eio.rw.Read(p)
 }
 
-func NewEncodingIO(rw io.ReadWriter) io.ReadWriter {
-	if charset.Name == "UTF-8" {
-		return rw
-	}
-	return &EncodingIO{rw}
-}
