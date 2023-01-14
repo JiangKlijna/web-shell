391e265c437f90b783424a9c6e8741254880ef8b
Tom <twitchyliquid64@ciphersink.net>
2023-01-14 12:07:19 -0800

    Fixes #4: use PASETO tokens for auth, support noauth

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/jiangklijna/web-shell
 go 1.14
 
 require (
+	aidanwoods.dev/go-paseto v1.2.0
 	github.com/gorilla/websocket v1.4.2
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/nsf/termbox-go v0.0.0-20200418040025-38ba6e5628f1
--- lib/function.go
+++ lib/function.go
@@ -2,26 +2,9 @@ package lib
 
 import (
 	"crypto/x509"
-	"fmt"
-	"hash"
 	"io/ioutil"
 	"log"
 )
-
-// HashCalculation calculat hash
-func HashCalculation(h hash.Hash, val string) string {
-	h.Write([]byte(val))
-	return fmt.Sprintf("%x", h.Sum(nil))
-}
-
-// ReverseString reverse string
-func ReverseString(s string) string {
-	runes := []rune(s)
-	for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
-		runes[from], runes[to] = runes[to], runes[from]
-	}
-	return string(runes)
-}
 
 // ReadCertPool get CertPool by crt file
 func ReadCertPool(crt string) *x509.CertPool {
